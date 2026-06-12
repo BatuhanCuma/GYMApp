6d00efeb693b054508a2b9b2ed9a71ad4b788dac
Friday <44293067+BatuhanCuma@users.noreply.github.com>
2026-06-12 14:38:48 +0300

    feat: dark theme + Provider setup in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/exercise_provider.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ExerciseProvider()..load(),
+      child: const GYMApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class GYMApp extends StatelessWidget {
+  const GYMApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GYMApp',
+      title: 'GYM Tracker',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFFBB86FC),
+          brightness: Brightness.dark,
+        ),
+        scaffoldBackgroundColor: const Color(0xFF121212),
         useMaterial3: true,
       ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('GYMApp'),
-        ),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
